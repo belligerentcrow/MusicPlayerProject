54f47032b19c6f529fdfcb80e21defac990ed6de
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-03-13 12:42:02 +0100

    Add more songs

--- ClientListener.java
+++ ClientListener.java
@@ -7,16 +7,23 @@ public class ClientListener {
 
         //creating two songs
         Song song1 = new Song("Don't Stop Me Now", "Queen", 5*60);
-        Song song2 = new Song("Doctor Who Opening 8 Season", "Murray Gold", 60);
+        Song song2 = new Song("Doctor Who Opening 9 Season", "Murray Gold", 60);
 
         //adding the songs to the instance of the library
         myLibrary.addSong(song1);
         myLibrary.addSong(song2);
 
         //creating two more songs
-        Song song3 = new Song("Smells Like Teen Spirit", "Nirvana", 3*60);
-        Song song4 = new Song("William Tell Overture", "Rossini", 8*60);
-        Song song5 = new Song("Colgera's Theme", "Tears of the Kingdom Soundtrack", 3*60);
+        Song song3 = new Song("Smells Like Teen Spirit", "Nirvana", 5*60+1);
+        Song song4 = new Song("William Tell Overture", "Rossini", 3*60+6);
+        Song song5 = new Song("Colgera's Theme", "Hajime Wakai", 3*60);
+        Song song6 = new Song("Bohemian Rhapsody", "Queen", 6*60);
+        Song song7 = new Song("Francesca", "Hozier", 4*60+30);
+        Song song8 = new Song("London Calling", "The Clash", 3*60+20);
+        Song song9 = new Song("Clara's Theme", "Murray Gold",3*60+24);
+        Song song10 = new Song("Another One Bites the Dust", "Queen", 3*60+34);
+
+
 
         //adding them to the library
         myLibrary.addSong(song3);
